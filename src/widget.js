d731aba0885a24e88d16645fdffd95a00d5f0d81
rowlandwilliams <Rowland@Matthews-MacBook-Air.local>
2021-01-16 16:38:22 +0000

    y label positioning

--- src/widget.js
+++ src/widget.js
@@ -62,8 +62,8 @@ fetch(url)
     			 ${decodeURIComponent(r.chart)}
     			 <div class="czw__graph__labels">\
 					 <div class="czw__graph__labels czw__graph__labels--y">\ 
-					 	 <span class="czw__graph__labels__label czw__graph__labels__label--czw_400">400 ppm</span>\
-    					 <span class="czw__graph__labels__label czw__graph__labels__label--czw_300">300 ppm</span>\
+					 	 <span class="czw__graph__labels__label czw_400">300 ppm</span>\
+    					 <span class="czw__graph__labels__label czw_300">400 ppm</span>\
     			 	</div>\ 
     				<div class="czw__graph__labels czw__graph__labels--x">\ 
     					 <span class="czw__graph__labels__label czw__graph__labels__label--first">1000 AD</span>\
@@ -99,6 +99,8 @@ fetch(url)
  		</div>`
     );
 
+    const svg = widget.getElementsByClassName("chart")[0];
+
     const selectBtns = widget.getElementsByClassName(
       "czw__graph__selector__text"
     );
@@ -111,15 +113,15 @@ fetch(url)
       });
     }
 
-    // const y400 = svg.getElementsByClassName("y400")[0];
-    // const y300 = svg.getElementsByClassName("y300")[0];
+    const y400 = svg.getElementsByClassName("y400")[0];
+    const y300 = svg.getElementsByClassName("y300")[0];
 
-    // const height = parseInt(svg.dataset.height);
+    const height = parseInt(svg.dataset.height);
 
-    // widget.getElementsByClassName("czw_400")[0].style.bottom =
-    //(parseInt(y400.getAttribute("y1")) / height) * 100 + "%";
-    // widget.getElementsByClassName("czw_300")[0].style.bottom =
-    //   (parseInt(y300.getAttribute("y1")) / height) * 100 + "%";
+    widget.getElementsByClassName("czw_300")[0].style.bottom =
+      (parseInt(y300.getAttribute("y1")) / height) * 100 + "%";
+    widget.getElementsByClassName("czw_400")[0].style.bottom =
+      (parseInt(y400.getAttribute("y1")) / height) * 100 + "%";
   })
   .catch(function (e) {
     // Handle error responses
