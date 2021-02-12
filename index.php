397640bb70b437f3bcb078e708621b121b57f822
rscoates <richard.coates@bytelondon.com>
2021-02-12 19:46:44 +0000

    Make title CO2 Widget

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
-    <title>Widget demo</title>
+    <title>CO2 Widget</title>
 </head>
 <body>
 <div class="para">
