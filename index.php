928988264ba53f125761ffe8c03174555e9b8ea7
rscoates <richard.coates@bytelondon.com>
2021-05-03 16:34:23 +0100

    Update Question Mark link

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
-    <meta property="og:url"                content="https://co2widget.com/" />
-    <meta property="og:type"               content="website" />
-    <meta property="og:title"              content="CO₂ Widget" />
-    <meta property="og:description"        content="CO₂ PPM Measurement Widget" />
-    <meta property="og:image"              content="https://co2widget.com/social-image.png" />
+    <meta property="og:url" content="https://co2widget.com/"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:title" content="CO₂ Widget"/>
+    <meta property="og:description" content="CO₂ PPM Measurement Widget"/>
+    <meta property="og:image" content="https://co2widget.com/social-image.png"/>
     <title>CO2 Widget</title>
 </head>
 <body>
@@ -18,38 +18,61 @@
 <div class="explanation-text">
     <div class="para">
         <h2>What is ppm?</h2>
-        <p>Just like we use metres to measures distances, we use ppm to measure CO<sub>2</sub> levels. "Ppm" means "parts per million". In other words, 400 ppm means that there are 400 particles of CO<sub>2</sub> for every million particles of air. This might not sound like a lot, but it has a big effect.</p>
+        <p>Just like we use metres to measures distances, we use ppm to measure CO<sub>2</sub> levels. "Ppm" means
+            "parts per million". In other words, 400 ppm means that there are 400 particles of CO<sub>2</sub> for every
+            million particles of air. This might not sound like a lot, but it has a big effect.</p>
     </div>
     <div class="para">
         <h2>Why does this exist?</h2>
-        <p>The global level of carbon dioxide and its rate of increase are two of the most important factors in deciding the future of our civilisation and life on Earth.</p>
-        <p>This widget exists to make these two ideas readily available to anyone in the world, be they a farmer, a lawyer, a child or a retiree.</p>
-        <p>One might argue that these numbers aren’t of relevance to some people, but then one could argue the same of the FTSE, S&P, Nikkei etc., and certainly the global CO<sub>2</sub> level is of much greater consequence. We also appreciate that this kind of numerical information does not necessarily motivate change when communicated in isolation, however it can form an extremely powerful element of a larger, blended communication strategy.</p>
-        <p>We believe that, considering its significance, this information needs to be household knowledge, and that it is a very curious thing that it still isn’t.</p>
-        <p>This is why we have made this widget embeddable, compact, colourful, easy to understand and free, so that news and weather publishers have every reason to use it, and no reason not to.</p>
+        <p>The global level of carbon dioxide and its rate of increase are two of the most important factors in deciding
+            the future of our civilisation and life on Earth.</p>
+        <p>This widget exists to make these two ideas readily available to anyone in the world, be they a farmer, a
+            lawyer, a child or a retiree.</p>
+        <p>One might argue that these numbers aren’t of relevance to some people, but then one could argue the same of
+            the FTSE, S&P, Nikkei etc., and certainly the global CO<sub>2</sub> level is of much greater consequence. We
+            also appreciate that this kind of numerical information does not necessarily motivate change when
+            communicated in isolation, however it can form an extremely powerful element of a larger, blended
+            communication strategy.</p>
+        <p>We believe that, considering its significance, this information needs to be household knowledge, and that it
+            is a very curious thing that it still isn’t.</p>
+        <p>This is why we have made this widget embeddable, compact, colourful, easy to understand and free, so that
+            news and weather publishers have every reason to use it, and no reason not to.</p>
     </div>
     <div class="para">
         <h2>How does it work?</h2>
-        <p>This widget updates daily and is collecting data from the Mauna Loa Observatory.  The measurements are coordinated by the Scripps Institution of Oceanography.  The Mauna Loa Observatory is operated by NOAA.</p>
+        <p>This widget updates daily and is collecting data from the Mauna Loa Observatory. The measurements are
+            coordinated by the Scripps Institution of Oceanography. The Mauna Loa Observatory is operated by NOAA.</p>
         <p>The <b>7-day average</b> box shows the average of the last 7 days including today. It updates every day.</p>
         <p>The <b>1000 years</b> graph shows the historical context of CO<sub>2</sub> levels. It updates every day.</p>
-        <p>The <b>20 years</b> graph shows the level of CO<sub>2</sub> over the past 20 years, to provide an indication as to whether the rate of increase is slowing down. It updates once annually and does not show a value for the current year.</p>
-        <p>The “<b>in last 2 years</b>” box shows the difference between the CO<sub>2</sub> level today compared to what it was on this same day two years ago. This key element of the widget provides a daily update on the progress the world is making in reducing the output of CO<sub>2</sub>.</p>
+        <p>The <b>20 years</b> graph shows the level of CO<sub>2</sub> over the past 20 years, to provide an indication
+            as to whether the rate of increase is slowing down. It updates once annually and does not show a value for
+            the current year.</p>
+        <p>The “<b>in last 2 years</b>” box shows the difference between the CO<sub>2</sub> level today compared to what
+            it was on this same day two years ago. This key element of the widget provides a daily update on the
+            progress the world is making in reducing the output of CO<sub>2</sub>.</p>
     </div>
     <div class="para">
         <h2>Who’s behind it?</h2>
-        <p>This project is led by <a href="http://www.matthewshribman.com/">Matthew Shribman</a> with the support of <a href="https://www.zero.cam.ac.uk/">Cambridge Zero / University of Cambridge</a>.</p>
-        <p>Developers: Chris Butterworth, Richard Coates, Rowland Williams, Keith Alexander, Erika Antiche Garzón, Max Palmer</p>
+        <p>This project is led by <a href="http://www.matthewshribman.com/">Matthew Shribman</a> with the support of <a
+                    href="https://www.zero.cam.ac.uk/">Cambridge Zero / University of Cambridge</a>.</p>
+        <p>Developers: Chris Butterworth, Richard Coates, Rowland Williams, Keith Alexander, Erika Antiche Garzón, Max
+            Palmer</p>
         <p>Designers: Bella Soares, Chris Butterworth</p>
         <p>Special thanks: Gary Fearnall</p>
-        <p>The widget is endorsed by the Grantham Institute, the 2 Degrees Institute, Scripps Institution of Oceanography at UC San Diego and Reading University’s Department of Meteorology.</p>
-        <p>Embed instructions and the original report, containing more details about the widget, its ideation and suggestions for further communication, are both available <a href="https://www.matthewshribman.com/live-co2">here</a>.</p>
+        <p>The widget is endorsed by the Grantham Institute, the 2 Degrees Institute, Scripps Institution of
+            Oceanography at UC San Diego and Reading University’s Department of Meteorology.</p>
+        <p>Embed instructions and the original report, containing more details about the widget, its ideation and
+            suggestions for further communication, are both available <a
+                    href="https://www.matthewshribman.com/live-co2">here</a>.</p>
     </div>
     <div class="para">
     </div>
-    <a href="https://co2widget.com"></a><div class="circleBase questionmark">
-        ?
-    </div></div>
+    <a href="https://co2widget.com">
+        <div class="circleBase questionmark">
+            ?
+        </div>
+    </a>
+</div>
 </div>
 <style>
     .circleBase {
