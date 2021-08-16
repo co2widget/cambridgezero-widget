25b2b581230a7a45c641e8ce2a7a69a501a9d39f
Hugh Simpson <hugh.simpson@babylonhealth.com>
2021-08-16 13:23:34 +0100

    formatting

--- server/import.php
+++ server/import.php
@@ -1,12 +1,12 @@
 <?php
- 
+
 class Import {
 
 	public static $url = 'https://www.climatelevels.org/graphs/co2-daily_data.php?callback=1';
 
 
 	public static function run($path = false) {
-		if (!$path){
+		if (!$path) {
 			return;
 		}
 
@@ -30,7 +30,7 @@ class Import {
 		$data = explode(',[', $resp);
 		// var_dump($data);
 		$change = Import::change($data);
-		// JSON encode for 
+		// JSON encode for
 		$return = json_encode([
 			'average' => Import::average($data),
 			'change' => $change,
@@ -50,30 +50,31 @@ class Import {
 	}
 
 	private static function average($data) {
-                
 		return floatval(number_format(Import::sevenDayMovingAverage(0, array_reverse($data)), 1, '.', ''));
-        }
-        private static function getCO2Float($str) {
-            return  floatval(array_pop(explode('),',$str)));
-        }
+	}
 
-        private static function mean($floats){
-            return array_sum($floats)/count($floats);
-        }
+	private static function getCO2Float($str) {
+		return floatval(array_pop(explode('),', $str)));
+	}
 
-        private static function sevenDayMovingAverage($startIndex, $data){
-            return Import::mean(array_map('Import::getCO2Float', array_slice($data, $startIndex, 7)));
-        }
-        private static function fourteenDayMovingAverage($startIndex, $data){
-            return Import::mean(array_map('Import::getCO2Float', array_slice($data, $startIndex, 14)));
-        }
+	private static function mean($floats) {
+		return array_sum($floats) / count($floats);
+	}
+
+	private static function sevenDayMovingAverage($startIndex, $data) {
+		return Import::mean(array_map('Import::getCO2Float', array_slice($data, $startIndex, 7)));
+	}
+
+	private static function fourteenDayMovingAverage($startIndex, $data) {
+		return Import::mean(array_map('Import::getCO2Float', array_slice($data, $startIndex, 14)));
+	}
 
 	private static function change($data) {
 		$data = array_reverse($data);
-                
-    		$latest = Import::fourteenDayMovingAverage(0, $data);
+
+		$latest = Import::fourteenDayMovingAverage(0, $data);
 		$twoYears = Import::fourteenDayMovingAverage(729, $data);
-                $change = $latest - $twoYears;
+		$change = $latest - $twoYears;
 
 		if ($change < 0) {
 			$char = '-';
@@ -82,7 +83,7 @@ class Import {
 
 		}
 		$change = number_format($change, 1, '.', '');
-		return $char.$change;
+		return $char . $change;
 	}
 
 	private static function chart($data) {
@@ -102,20 +103,20 @@ class Import {
 				floatval($record[3])
 			];
 
-			
+
 			// Logic to ensure each value is each 5 years
 			if ($x < 164) {
-				$x++;	
+				$x++;
 			} elseif ($x == 164) {
 				$x = 168;
 			} elseif ($x >= 164 && $x <= 283) {
-				$x +=5; 
+				$x += 5;
 			} elseif ($x > 283 && $x < 364) {
 				$x = 364;
 			} elseif ($x >= 364 && $x <= 2964) {
 				$x += 260;
 			} else {
-				$x += 365*5;
+				$x += 365 * 5;
 			}
 		}
 
@@ -149,13 +150,19 @@ class Import {
 		$y300 = $height - (300 - $first[1]) - $offset;
 		$y400 = $height - (400 - $first[1]) - $offset;
 
-		$y300 = "<line class=\"y300\" x1=\"0\" x2=\"${width}\" y1=\"${y300}\" y2=\"${y300}\" stroke=\"#d0d0d0\" stroke-width=\"1\" vector-effect=\"non-scaling-stroke\" id=\"y300\" stroke-dasharray=\"5,5\"></line>";
+		$y300 = "<line class=\"y300\" x1=\"0\" x2=\"${width}\" y1=\"${y300}\" y2=\"${y300}\" stroke=\"#d0d0d0\" "
+			. "stroke-width=\"1\" vector-effect=\"non-scaling-stroke\" id=\"y300\" stroke-dasharray=\"5,5\"></line>";
 
-		$y400 = "<line class=\"y400\" x1=\"0\" x2=\"${width}\" y1=\"${y400}\" y2=\"${y400}\" stroke=\"#d0d0d0\" stroke-width=\"1\" vector-effect=\"non-scaling-stroke\"id=\"y400\" stroke-dasharray=\"5,5\"></line>";
+		$y400 = "<line class=\"y400\" x1=\"0\" x2=\"${width}\" y1=\"${y400}\" y2=\"${y400}\" stroke=\"#d0d0d0\" "
+			. "stroke-width=\"1\" vector-effect=\"non-scaling-stroke\"id=\"y400\" stroke-dasharray=\"5,5\"></line>";
 
 		$xaxis = "<line x1=\"0\" x2=\"${width}\" y1=\"${height}\" y2=\"${height}\" stroke=\"none\" vector-effect=\"non-scaling-stroke\"id=\"y400\"></line>";
 
-		ob_start(); ?><svg viewBox="0 0 <?= $width; ?> <?= $height; ?>" data-height="<?= $height; ?>" class="chart2000" preserveAspectRatio="none"><?= $y300; ?><?= $y400; ?><?= $xaxis; ?><polyline fill="none" stroke="#d97400" stroke-width="4" points="<?= implode(' ', $polyline); ?>" vector-effect="non-scaling-stroke" stroke-linecap="round"></polyline></svg><?php 
+		ob_start(); ?>
+        <svg viewBox="0 0 <?= $width; ?> <?= $height; ?>" data-height="<?= $height; ?>" class="chart2000"
+             preserveAspectRatio="none"><?= $y300; ?><?= $y400; ?><?= $xaxis; ?>
+        <polyline fill="none" stroke="#d97400" stroke-width="4" points="<?= implode(' ', $polyline); ?>"
+                  vector-effect="non-scaling-stroke" stroke-linecap="round"></polyline></svg><?php
 		return str_replace('+', ' ', urlencode(ob_get_clean()));
 	}
 
@@ -178,73 +185,72 @@ class Import {
 			array_push($points, $temp);
 
 			$x++;
-			
+
 		}
 
 		// filter points to get only last 20 years
-		$data20 = array_filter($points, function($v) use ($years) {
+		$data20 = array_filter($points, function ($v) use ($years) {
 			return in_array($v['year'], $years);
 		});
 
 		// sum values for each of 20 years and divide by respective counts to get avg
-                $co2byyear = array();
-                foreach ($data20 as $val) {
-                    $co2byyear[$val['year']][] = $val['value'];
-                }
-                $points = array();
-                foreach($co2byyear as $year => $vals){
-                    $points[] = array("year" => $year, "avg" => Import::mean($vals));
-                }
-		
+		$co2byyear = array();
+		foreach ($data20 as $val) {
+			$co2byyear[$val['year']][] = $val['value'];
+		}
+		$points = array();
+		foreach ($co2byyear as $year => $vals) {
+			$points[] = array("year" => $year, "avg" => Import::mean($vals));
+		}
+
 		$offset = 1; // Prevent negative results
 
 		$first = $points[0]['avg'];
 
 		// $_SERVER
 		// echo $first; die();
-		
+
 		$last = end($points);
-                $last = $last['avg'];
-                reset($points);
+		$last = $last['avg'];
+		reset($points);
 
 		$width = count($points); // based on number of years
 		$wp = $width / 100;
-		
+
 		$height = $last - $first;
 		$hp = $height / 100;
 		// var_dump($points);
-		
+
 		$x = 1; // start at 1 to fit on graph - needs a look
-		
+
 		$polyline = [];
 		foreach ($points as $point) {
 			// $y = $height - ($point['avg'] - $first);
 			$y = (($point['avg'] - $first) / ($last - $first));
 			$y = $y * 100;
-			$left = ((($x/20) * 5) * 20);
+			$left = ((($x / 20) * 5) * 20);
 			$bottom = $y;
 			$polyline[] = "<div class=\"chart20__dot\" style=\"left:${left}%;bottom:${bottom}%\" data-avg=\"${point['avg']}\" data-year=\"${point['year']}\"></div>";
 			$x++;
 		}
 
 
-
 		$xaxis = "<line x1=\"0\" x2=\"${width}\" y1=\"${height}\" y2=\"${height}\" stroke=\"none\" vector-effect=\"non-scaling-stroke\"id=\"y400\"></line>";
 
 		$y400 = ((400 - $first) / ($last - $first));
-                $y380 = ((380 - $first) / ($last - $first));
-		
-		ob_start(); ?><div class="chart20">
-			<div class="chart20__xaxis"></div>
-			<div class="chart20__yaxis"></div>
-			<div class="chart20__400" style="bottom:<?= $y400*100; ?>%"></div>
-                        <div class="chart20__380" style="bottom:<?= $y380*100; ?>%"></div>
-			<?= implode(' ', $polyline); ?>
-		</div><?php 
+		$y380 = ((380 - $first) / ($last - $first));
+
+		ob_start(); ?>
+        <div class="chart20">
+        <div class="chart20__xaxis"></div>
+        <div class="chart20__yaxis"></div>
+        <div class="chart20__400" style="bottom:<?= $y400 * 100; ?>%"></div>
+        <div class="chart20__380" style="bottom:<?= $y380 * 100; ?>%"></div>
+		<?= implode(' ', $polyline); ?>
+        </div><?php
 		return str_replace('+', ' ', urlencode(ob_get_clean()));
 
-		
-		
+
 	}
 
 
@@ -259,12 +265,12 @@ class Import {
 		if ($change < -10) {
 			$change = -10;
 		}
-		return (225/200) * $change * 10;
-		
+		return (225 / 200) * $change * 10;
+
 	}
 
 	private static function save($path, $return) {
-		file_put_contents(__DIR__.'/../'.$path, $return);
+		file_put_contents(__DIR__ . '/../' . $path, $return);
 		exec('npm run build');
 	}
 }
